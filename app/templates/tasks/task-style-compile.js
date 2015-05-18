4154c2f5865d6e50ac24dd95d9839732448753ac
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-18 20:11:11 +0530

    Remove dependency on blacklisted gem, gulp-compass.

--- app/templates/tasks/task-style-compile.js
+++ app/templates/tasks/task-style-compile.js
@@ -3,32 +3,34 @@ var CFG    = require("./utils/config.js");
 var $      = require("gulp-load-plugins")();
 var path   = require("path");
 var pkg    = require(path.join("..", CFG.FILE.config.pkg));
-var argv   = require("yargs").argv;
+var exec   = require("child_process").exec;
 var notify = require("./utils/notify-style-compile");
 
 /**
  * style:compile
- * @see www.npmjs.org/package/gulp-compass
  * @see compass-style.org
  * @see compass-style.org/help/tutorials/configuration-reference
  */
-gulp.task("style:compile", ["style:lint"], function() {
-  return gulp.src([
-      path.join(CFG.DIR.src, "**", "*." + CFG.FILE.extension.style.scss)
-    ])
-    .pipe($.compass({
-      bundle_exec: true, // Use the bundle'd gem instead of the global system gem to ensure that we use the version defined in our `Gemfile`.
-      config_file: CFG.FILE.config.compass,
-      css: path.join(CFG.DIR.dist, CFG.DIR.style),
-      sass: path.join(CFG.DIR.src, CFG.DIR.style)
-    }))
-    .on("error", function(error) {
-      if("undefined" !== typeof(process.env.UI_ELEMENTS_CI)) {
-        // Running inside a CI environment, break the build
-        throw new Error(error);
-      } else {
-        // Running inside a non-CI environment, show notifications
-        notify(error);
-      }
+gulp.task("style:compile", ["style:lint"], function(callback) {
+  var compassProcess = exec([
+    "bundle",
+    "exec",
+    "compass",
+    "compile",
+    "--config",
+    path.join(CFG.FILE.config.compass)
+  ].join(" "), function(err, stdout, stderr) {
+    $.util.log("[style:compile] stdout:", stdout);
+    $.util.log("[style:compile] stderr: ", stderr);
+
+    notify({
+      message: "Compilation complete!"
     });
+
+    if(null !== err) {
+      $.util.log("[style:compile] err: ", err);
+    }
+
+    callback();
+  });
 });
