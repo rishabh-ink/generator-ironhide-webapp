f4ee0c2eebe7a8087b7e6847bc889d9fdc0e88ea
Rishabh Rao <rishabhsrao@gmail.com>
2015-11-11 09:15:08 +0530

    Remove the space after the function keyword.

--- test/test-app.js
+++ test/test-app.js
@@ -5,15 +5,15 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('ironhide-webapp:app', function () {
-  before(function (done) {
+describe('ironhide-webapp:app', function() {
+  before(function(done) {
     helpers.run(path.join(__dirname, '../app'))
       .withOptions({ skipInstall: true })
       .withPrompts({ appname: 'ironhide-webapp-test-app' })
       .on('end', done);
   });
 
-  it('Should create files', function () {
+  it('Should create files', function() {
     assert.file([
       'src',
       'tasks',
